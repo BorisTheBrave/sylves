6347d097293b209f8284b47cb5e0562c3cc7adb0
Boris The Brave <boris@boristhebrave.com>
2022-02-09 21:56:45 +0000

    Add Test project.

---
+++ src/Sylves.Test/UnityShim/Vector2Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylves.Test
+{
+    [TestFixture]
+    public class Vector2Test
+    {
+        [Test]
+        public void TestSignedAngle()
+        {
+            Assert.AreEqual(90, Vector2.SignedAngle(Vector2.right, Vector2.up));
+        }
+    }
+}
