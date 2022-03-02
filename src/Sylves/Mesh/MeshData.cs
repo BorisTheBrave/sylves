d96751d6c9d070c6dee7edaf8f31f8e17d2548f7
Boris The Brave <boris@boristhebrave.com>
2022-03-02 23:02:41 +0000

    Add RecalculateNormals.

--- src/Sylves/Mesh/MeshData.cs
+++ src/Sylves/Mesh/MeshData.cs
@@ -45,5 +45,39 @@ namespace Sylves
         {
             return topologies[submesh];
         }
+
+        public void RecalculateNormals()
+        {
+            if (normals == null || normals.Length != vertices.Length)
+            {
+                normals = new Vector3[vertices.Length];
+            }
+
+            // TODO: There's got to be an official way of doing this.
+            // Implementation below is just a guess
+            for (var submesh = 0; submesh < subMeshCount; submesh++)
+            {
+                foreach (var face in MeshUtils.GetFaces(this, submesh))
+                {
+                    for(var i=0;i<face.Count;i++)
+                    {
+                        var i0 = face[i];
+                        var i1 = face[(i + 1) % face.Count];
+                        var i2 = face[(i + 2) % face.Count];
+                        var v0 = vertices[i0];
+                        var v1 = vertices[i1];
+                        var v2 = vertices[i2];
+                        normals[i1] += Vector3.Cross(v2 - v1, v0 - v1).normalized;
+                    }
+
+                }
+            }
+            for(var i = 0; i < normals.Length; i++)
+            {
+                normals[i] = normals[i].normalized;
+            }
+
+        }
+        //public void RecalculateTangents();
     }
 }
