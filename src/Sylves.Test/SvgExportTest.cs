ae7a45f575eb9421143f5097cdaa854c537e9755
Boris The Brave <boris@boristhebrave.com>
2022-05-26 22:37:40 +0100

    SvgExport

---
+++ src/Sylves.Test/SvgExportTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylves.Test
+{
+    [TestFixture]
+    internal class SvgExportTest
+    {
+        public void Export(IGrid g, string filename)
+        {
+            using (var file = File.Open(filename, FileMode.Create))
+            using (var writer = new StreamWriter(file))
+            {
+                SvgExport.WriteGrid(g, writer);
+            }
+        }
+
+        [Test]
+        public void ExportGrids()
+        {
+            Export(
+                new SquareGrid(1).BoundBy(new SquareBound(new Vector2Int(0, 0), new Vector2Int(10, 10))),
+                "square.svg");
+            Export(
+                new HexGrid(1, HexOrientation.PointyTopped).BoundBy(HexBound.Hexagon(10)),
+                "hex_pt.svg");
+            Export(
+                new HexGrid(1, HexOrientation.FlatTopped).BoundBy(HexBound.Hexagon(1)),
+                "hex_ft.svg");
+            Export(
+                new TriangleGrid(1, TriangleOrientation.FlatSides).BoundBy(TriangleBound.Hexagon(3)),
+                "tri_fs.svg");
+            Export(
+                new TriangleGrid(1, TriangleOrientation.FlatTopped).BoundBy(TriangleBound.Hexagon(3)),
+                "tri_ft.svg");
+        }
+    }
+}
---
+++ src/Sylves/Export/SvgExport.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+#if UNITY
+using UnityEngine;
+#endif
+
+namespace Sylves
+{
+    public static class SvgExport
+    {
+        private static void WritePathCommands(Vector3[] vertices, Matrix4x4 transform, TextWriter tw)
+        {
+            var first = true;
+            foreach(var v in vertices)
+            {
+                var v2 = transform.MultiplyPoint3x4(v);
+                    tw.Write(first ? 'M' : 'L');
+                first = false;
+                tw.Write(v2.x);
+                tw.Write(' ');
+                tw.Write(v2.y);
+            }
+            tw.Write('Z');
+        }
+
+        public static void WriteGrid(IGrid grid, TextWriter tw)
+        {
+            tw.Write("<svg viewBox=\"-10 -10 20 20\" xmlns=\"http://www.w3.org/2000/svg\">");
+            foreach (var cell in grid.GetCells())
+            {
+                grid.GetPolygon(cell, out var vertices, out var transform);
+                tw.WriteLine($"<!-- {cell} -->");
+                tw.Write("<path fill=\"none\" stroke=\"black\" stroke-width=\"0.1\" d=\"");
+                WritePathCommands(vertices, transform, tw);
+                tw.WriteLine("\"/>");
+            }
+            tw.Write("</svg>");
+        }
+    }
+}
