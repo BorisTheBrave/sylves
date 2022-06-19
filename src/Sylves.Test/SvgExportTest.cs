ad9e3edc0706c249e310641b556e5f8c29f964e3
Boris The Brave <boris@boristhebrave.com>
2022-06-19 21:20:53 +0100

    Improve SvgExport.

--- src/Sylves.Test/SvgExportTest.cs
+++ src/Sylves.Test/SvgExportTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using System.IO;
 #if UNITY
 using UnityEngine;
@@ -9,62 +10,66 @@ namespace Sylves.Test
     [TestFixture]
     internal class SvgExportTest
     {
-        public void Export(IGrid g, string filename)
+        public class Options
         {
-            using (var file = File.Open(filename, FileMode.Create))
+            public int dim = 3;
+            public double textScale = 1;
+        }
+
+        public static void WriteGrid(IGrid grid, TextWriter tw, Options options)
+        {
+            var b = new SvgBuilder(tw);
+            b.BeginSvg();
+            foreach (var cell in grid.GetCells())
+            {
+                b.DrawCell(grid, cell);
+            }
+            foreach (var cell in grid.GetCells())
+            {
+                b.DrawCoordinateLabel(grid, cell, options.dim, options.textScale);
+            }
+            b.EndSvg();
+        }
+
+        public void Export(IGrid g, string filename, Options options = null)
+        {
+            var fullPath = Path.GetFullPath(filename);
+            using (var file = File.Open(fullPath, FileMode.Create))
             using (var writer = new StreamWriter(file))
             {
-                SvgExport.WriteGrid(g, writer);
+                WriteGrid(g, writer, options ?? new Options());
+                Console.WriteLine($"Wrote file {fullPath}");
             }
         }
 
-        private IGrid TriHexGrid()
-        {
-            var meshData = new MeshData();
-            meshData.vertices = new Vector3[]
-            {
-                new Vector3(0.5f, 0, 0),
-                new Vector3(0.25f, 0.5f, 0),
-                new Vector3(-0.25f, 0.5f, 0),
-                new Vector3(-0.5f, 0, 0),
-                new Vector3(-0.25f, -0.5f, 0),
-                new Vector3(0.25f, -0.5f, 0),
-                new Vector3(0.75f, 0.5f, 0),
-                new Vector3(0.75f, -0.5f, 0),
-            };
-            meshData.indices = new[]{new []
-            {
-                0, 1, 2, 3, 4, ~5,
-                6, 1, ~0,
-                7, 0, ~5,
-            } };
-            meshData.subMeshCount = 1;
-            meshData.topologies = new[] { MeshTopology.NGon };
-            return new PeriodicPlanarMeshGrid(meshData, new Vector2(1.0f, 0), new Vector2(0.5f, 1.0f))
-                .BoundBy(new SquareBound(new Vector2Int(-5, -5), new Vector2Int(6, 6)));
-        }
 
         [Test]
         public void ExportGrids()
         {
             Export(
                 new SquareGrid(1).BoundBy(new SquareBound(new Vector2Int(0, 0), new Vector2Int(10, 10))),
-                "square.svg");
+                "square.svg",
+                new Options { dim = 2});
             Export(
                 new HexGrid(1, HexOrientation.PointyTopped).BoundBy(HexBound.Hexagon(10)),
-                "hex_pt.svg");
+                "hex_pt.svg",
+                new Options { textScale = 0.7 });
             Export(
                 new HexGrid(1, HexOrientation.FlatTopped).BoundBy(HexBound.Hexagon(1)),
-                "hex_ft.svg");
+                "hex_ft.svg",
+                new Options { textScale = 0.7 });
             Export(
                 new TriangleGrid(1, TriangleOrientation.FlatSides).BoundBy(TriangleBound.Hexagon(3)),
-                "tri_fs.svg");
+                "tri_fs.svg",
+                new Options { textScale = 0.7 });
             Export(
                 new TriangleGrid(1, TriangleOrientation.FlatTopped).BoundBy(TriangleBound.Hexagon(3)),
-                "tri_ft.svg");
+                "tri_ft.svg",
+                new Options { textScale = 0.7 });
             Export(
-                TriHexGrid(),
-                "trihex.svg");
+                new TriHexGrid().BoundBy(new SquareBound(new Vector2Int(-5, -5), new Vector2Int(6, 6))),
+                "trihex.svg",
+                new Options { });
         }
     }
 }
--- src/Sylves/Export/SvgExport.cs
+++ src/Sylves/Export/SvgExport.cs
@@ -10,7 +10,7 @@ namespace Sylves
 {
     public static class SvgExport
     {
-        private static void WritePathCommands(Vector3[] vertices, Matrix4x4 transform, TextWriter tw)
+        internal static void WritePathCommands(Vector3[] vertices, Matrix4x4 transform, TextWriter tw)
         {
             var first = true;
             foreach(var v in vertices)
@@ -24,19 +24,61 @@ namespace Sylves
             }
             tw.Write('Z');
         }
+    }
 
-        public static void WriteGrid(IGrid grid, TextWriter tw)
+    public class SvgBuilder
+    {
+        TextWriter tw;
+
+        public SvgBuilder(TextWriter tw)
         {
-            tw.Write("<svg viewBox=\"-10 -10 20 20\" xmlns=\"http://www.w3.org/2000/svg\">");
-            foreach (var cell in grid.GetCells())
-            {
-                grid.GetPolygon(cell, out var vertices, out var transform);
-                tw.WriteLine($"<!-- {cell} -->");
-                tw.Write("<path fill=\"none\" stroke=\"black\" stroke-width=\"0.1\" d=\"");
-                WritePathCommands(vertices, transform, tw);
-                tw.WriteLine("\"/>");
-            }
+            this.tw = tw;
+        }
+
+        public TextWriter TextWriter => tw;
+
+        public void BeginSvg(string viewBox= "-5 -5 10 10")
+        {
+            tw.Write($"<svg viewBox=\"{viewBox}\" xmlns=\"http://www.w3.org/2000/svg\">");
+        }
+
+        public void EndSvg()
+        {
             tw.Write("</svg>");
+        }
+
+        public void DrawCell(IGrid grid, Cell cell)
+        {
+            var cellPolyStyle = "fill: rgb(244, 244, 241); stroke: rgb(51, 51, 51); stroke-width: 0.1";
+
+            grid.GetPolygon(cell, out var vertices, out var transform);
+            tw.WriteLine($"<!-- {cell} -->");
+            tw.Write($@"<path style=""{cellPolyStyle}"" d=""");
+            SvgExport.WritePathCommands(vertices, transform, tw);
+            tw.WriteLine("\"/>");
+        }
+
+        public void DrawCoordinateLabel(IGrid grid, Cell cell, int dim = 3, double textScale = 1.0)
+        {
+            // Style is hard coded for now
+            var stroke_text_style = "fill: rgb(51, 51, 51); font-size: 0.3px;stroke: white; stroke-width: 0.05";
+            var text_style = @"fill: rgb(51, 51, 51); font-size: 0.3px;";
+            var xs = @"style=""fill: hsl(90, 100%, 35%); font-weight: bold"" ";
+            var ys = @"style=""fill: hsl(300, 80%, 50%); font-weight: bold"" ";
+            var zs = @"style=""fill: hsl(200, 100%, 45%); font-weight: bold"" ";
+
+            var cellCenter = grid.GetCellCenter(cell);
+            tw.WriteLine($@"<g transform=""translate({ cellCenter.x},{ cellCenter.y + 0.08}) scale({textScale})"">");
+            foreach (var textStyle in new[] { stroke_text_style, text_style })
+            {
+                tw.Write($@"<text text-anchor=""middle"" alignment-baseline=""middle"" style=""{ textStyle}"">");
+                tw.Write($@"<tspan {xs}>{cell.x}</tspan>, <tspan {ys}>{cell.y}</tspan>");
+                if (dim >= 3) {
+                    tw.Write($@", <tspan {zs}>{cell.z}</tspan>");
+                }
+                tw.WriteLine($@"</text>");
+            }
+            tw.WriteLine("</g>");
         }
     }
 }
