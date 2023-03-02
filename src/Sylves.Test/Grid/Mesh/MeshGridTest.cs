96a0374034ee56a57c4bffa065fdccc45ac5c4b3
Boris The Brave <boris@boristhebrave.com>
2023-03-02 22:44:00 +0000

    Add test

--- src/Sylves.Test/Grid/Mesh/MeshGridTest.cs
+++ src/Sylves.Test/Grid/Mesh/MeshGridTest.cs
@@ -207,5 +207,17 @@ namespace Sylves.Test
 
             GridTest.FindCell(g, new Cell());
         }
+
+        // Test precision issues far from the origin
+        [Test]
+        public void TestFar()
+        {
+            var g = new UnrelaxedTownscaperGrid(4, 1e-2f).BoundBy(new SquareBound(38, -1, 39, 0));
+            foreach (var c in g.GetCells())
+            {
+                Assert.IsFalse(float.IsNaN(g.GetCellCenter(c).x), c.ToString());
+            }
+
+        }
     }
 }
