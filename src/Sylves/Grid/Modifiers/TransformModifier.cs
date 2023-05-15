eb1e97956be2691168ada5f895c1523ec9ff04de
Boris The Brave <boris@boristhebrave.com>
2023-05-15 17:32:22 +0100

    Fix TransformModifier raycast.

--- src/Sylves/Grid/Modifiers/TransformModifier.cs
+++ src/Sylves/Grid/Modifiers/TransformModifier.cs
@@ -92,7 +92,7 @@ namespace Sylves
         public override IEnumerable<RaycastInfo> Raycast(Vector3 origin, Vector3 direction, float maxDistance = float.PositiveInfinity)
         {
             origin = iTransform.MultiplyPoint3x4(origin);
-            direction = iTransform.MultiplyPoint3x4(direction);
+            direction = iTransform.MultiplyVector(direction);
             // TODO: Worry about maxDistance?
             foreach(var info in Underlying.Raycast(origin, direction, maxDistance))
             {
