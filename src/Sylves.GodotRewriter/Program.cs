ec4bdba1fbf9f1b4e6b94a6f9e14e42f13a826df
Boris The Brave <boris@boristhebrave.com>
2023-09-23 19:57:49 +0100

    Add Sylves.GodotRewriter.

---
+++ src/Sylves.GodotRewriter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.MSBuild;
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+return;
+
+MSBuildLocator.RegisterDefaults();
+
+using var workspace = MSBuildWorkspace.Create();
+var project = await workspace.OpenProjectAsync("..\\Sylves\\Sylves.csproj");
+project = project.WithParseOptions(((CSharpParseOptions)project.ParseOptions).WithPreprocessorSymbols("GODOT"));
+var compilation = await project.GetCompilationAsync();
+
+foreach (var st in compilation.SyntaxTrees)
+{
+    Console.WriteLine($"Processing {st.FilePath}");
+    var dest = st.FilePath.Replace("src\\Sylves\\", "src\\Sylves.Godot\\");
+    if (dest == st.FilePath) continue;
+    if (UnityToGodotRewriter.ExcludeFiles.Any(x => dest.EndsWith("\\" + x))) continue;
+
+    var model = compilation.GetSemanticModel(st);
+
+    var s = st.GetCompilationUnitRoot();
+
+    var rw = new UnityToGodotRewriter(model);
+    var s2 = rw.Visit(s);
+    Directory.CreateDirectory(Path.GetDirectoryName(dest));
+    using var file = File.Create(dest);
+    await file.WriteAsync(st.Encoding.GetBytes(s2.ToFullString()));
+}
