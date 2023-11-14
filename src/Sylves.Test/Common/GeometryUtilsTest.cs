f8519d48f2c77eb388d842278e47b22ebf4a1966
Boris The Brave <boris@boristhebrave.com>
2023-11-14 22:28:25 +0000

    Fix IsPointInTriangle.

--- src/Sylves.Test/Common/GeometryUtilsTest.cs
+++ src/Sylves.Test/Common/GeometryUtilsTest.cs
@@ -25,12 +25,19 @@ namespace Sylves.Test
             Assert.IsTrue(GeometryUtils.IsPointInTriangle(p, v3, v2, v1));
             Assert.IsFalse(GeometryUtils.IsPointInTriangle(p2, v1, v2, v3));
             Assert.IsFalse(GeometryUtils.IsPointInTriangle(p2, v3, v2, v1));
-
+            
             // collinear
             p = new Vector3(0, -1.5f, 0);
             v1 = new Vector3(0, -1f, 0);
             v2 = new Vector3(0, 0f, 0);
             v3 = new Vector3(0, 1f, 0);
+            Assert.IsFalse(GeometryUtils.IsPointInTriangle(p, v1, v2, v3));
+
+            // backfacing
+            p = new Vector3(0.5f, 0, 0);
+            v1 = new Vector3(-0.5f, -0.5f, 0.5f);
+            v2 = new Vector3(-0.5f, 0.5f, 0.5f);
+            v3 = new Vector3(-0.5f, 0.5f, -0.5f);
             Assert.IsFalse(GeometryUtils.IsPointInTriangle(p, v1, v2, v3));
         }
 
