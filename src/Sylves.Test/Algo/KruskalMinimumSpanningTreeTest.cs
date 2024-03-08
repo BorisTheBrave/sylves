4956cae454c23077db2bf85a360e7191d4a52bce
Boris The Brave <boris@boristhebrave.com>
2024-03-08 21:24:55 +0000

    Add KruskalMinimumSpanningTree.

---
+++ src/Sylves.Test/Algo/KruskalMinimumSpanningTreeTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylves.Test
+{
+    public class KruskalMinimumSpanningTreeTest
+    {
+        [Test]
+        public void TestSquareGrid()
+        {
+            var g = new SquareGrid(1, new SquareBound(0, 0, 10, 10));
+            var tree = KruskalMinimumSpanningTree.Calculate(g, _ => 1).ToList();
+
+            // Check minimal
+            Assert.AreEqual(9 * 10 + 9, tree.Count);
+
+            // Check spanning
+            var d = Pathfinding.FindDistances(g, new Cell(), stepLengths: s => tree.Any(x=>x.Src == s.Dest && x.Dest == s.Src || x.Src == s.Src && x.Dest == s.Dest) ? 1 : (float?)null);
+            Assert.AreEqual(g.GetCells().Count(), d.Count);
+        }
+    }
+}
---
+++ src/Sylves/Algo/KruskalMinimumSpanningTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sylves
+{
+    public static class KruskalMinimumSpanningTree
+    {
+        public static bool LexCompare(Cell a, Cell b)
+        {
+            if (a.x < b.x) return true;
+            if (a.x > b.x) return false;
+            if (a.y < b.y) return true;
+            if (a.y > b.y) return false;
+            if (a.z < b.z) return true;
+            if (a.z > b.z) return false;
+            return false;
+        }
+
+        private class Data
+        {
+            public Cell parent;
+            public int rank;
+        }
+
+        public static IEnumerable<Step> Calculate(IGrid grid, Func<Step, float?> stepLengths)
+        {
+            // Disjoint set data structure
+            var data = new Dictionary<Cell, Data>();
+            foreach (var cell in grid.GetCells())
+                data[cell] = new Data { parent = cell };
+            Cell FindSet(Cell cell)
+            {
+                Data d;
+                while ((d = data[cell]).parent!= cell)
+                {
+                    (cell, d.parent) = (d.parent, data[d.parent].parent);
+                }
+                return cell;
+            }
+            void Union(Cell x, Cell y)
+            {
+                // Replace nodes by roots
+                x = FindSet(x);
+                y = FindSet(y);
+
+                if (x == y) return;
+
+                var dx = data[x];
+                var dy = data[y];
+
+                // If necessary, swap variables to ensure that
+                // x has at least as many descendants as y
+                if (data[x].rank < data[y].rank)
+                {
+                    (x, y) = (y, x);
+                    (dx, dy) = (dy, dx);
+                }
+
+
+                // Make x the new root
+                dy.parent = x;
+                // Update the size of x
+                if (dx.rank == dy.rank)
+                    dx.rank += 1;
+            }
+
+
+            var steps = grid.GetCells()
+                // Collect all steps in the grid
+                .SelectMany(cell => grid.GetCellDirs(cell).Select(d => Step.Create(grid, cell, d, stepLengths)))
+                .OfType<Step>()
+                // De-duplicate step and inverse step
+                .Where(x=> LexCompare(x.Src, x.Dest))
+                .OrderBy(x => x.Length)
+                // Sort by smallest length
+                .ToList();
+            foreach(var step in steps)
+            {
+                if(FindSet(step.Src) != FindSet(step.Dest))
+                {
+                    yield return step;
+                    Union(step.Src, step.Dest);
+                }
+            }
+        }
+    }
+}
