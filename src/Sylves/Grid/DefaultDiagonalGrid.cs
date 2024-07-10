7bc6e3fa637bb694d2f88013c9ba8d9432c99351
Boris The Brave <boris@boristhebrave.com>
2024-07-10 21:14:48 +0100

    Namespace fixes

--- src/Sylves/Grid/DefaultDiagonalGrid.cs
+++ src/Sylves/Grid/DefaultDiagonalGrid.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Sylves.Grid
+namespace Sylves
 {
     internal class DefaultDiagonalGrid : BaseModifier
     {
--- src/Sylves/Grid/Triangle/TriangleGrid.cs
+++ src/Sylves/Grid/Triangle/TriangleGrid.cs
@@ -1,5 +1,4 @@
-﻿using Sylves.Grid;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 #if UNITY
