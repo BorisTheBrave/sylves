87ef03614e174b5deb9770661d71ccf360192bd2
Boris The Brave <boris@boristhebrave.com>
2025-02-17 22:21:02 +0000

    More tests

---
+++ src/Sylves.Test/Grid/DefaultDualMappingTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylves.Test
+{
+    internal class DefaultDualMappingTest
+    {
+        [Test]
+        public void TestArc()
+        {
+            var meshData = new MeshData
+            {
+                vertices = new [] { 
+                    new Vector3(100, 100, 0), new Vector3(100, 101, 0), new Vector3(101,101, 0), new Vector3(101, 100, 0), 
+                    new Vector3(101, 99, 0), new Vector3(100, 99, 0),
+                    new Vector3(99, 100, 0), new Vector3(99, 101, 0),
+                    new Vector3(100, 102, 0), new Vector3(101, 102, 0),
+                    new Vector3(102, 100, 0), new Vector3(102, 101, 0),
+                },
+                indices = new[] { new int[] { 
+                    0, 1, 2, ~3, 
+                    5, 0, 3, ~4,
+                    6, 7, 1, ~0,
+                    1, 8, 9, ~2,
+                    3, 2, 11, ~10,
+                } },
+                topologies = new[] { MeshTopology.NGon },
+            };
+            var grid = new MeshGrid(meshData);
+            var dual = new DefaultDualMapping(grid, 1000, CachePolicy.Always);
+            Assert.AreEqual(12, dual.DualGrid.GetCells().Count());
+            GridTest.DualMapping(dual, new Cell());
+        }
+    }
+}
---
+++ src/Sylves.Test/Grid/Modifiers/XZModifierTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sylves.Test.Grid.Modifiers
+{
+    [TestFixture]
+    internal class XZModifierTest
+    {
+        [Test]
+        public void Asdf()
+        {
+            var g = new XZModifier(new SquareGrid(1, new SquareBound(-10, -10, 10, 10)));
+            var l = g.GetCells().Take(100).ToList();
+            var a= g.GetCellCenter(new Cell(3, -9, 0));
+            var b = g.FindCell(a, out var cell);
+            Assert.IsNotNull(cell);
+        }
+    }
+}
